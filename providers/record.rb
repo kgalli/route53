a3bba1c5d787a333db34aeaafb5960f940ea705d
kgalli <mail@kgalli.de>
2015-08-20 12:57:34 +0200

    Add record provider
    This provider uses the route53_a_record gem
    to provide the create, update and delete
    functionality.

---
+++ providers/record.rb
@@ -0,0 +1,34 @@
+# to shorten things a little bit
+def r
+  new_resource
+end
+
+def record
+  Route53ARecord::Record.new(r.zone_id, r.name, r.value, r.ttl)
+end
+
+def handler
+  Route53ARecord::Handler.new(r.region, r.aws_access_key_id, r.aws_secret_key, r.overwrite)
+end
+
+action :create do
+  require 'route53_a_record'
+
+  begin
+    handler.create(record)
+    Chef::Log.debug("Created record: #{record.inspect}")
+  rescue Exception => e
+    Chef::Log.error e.to_s
+  end
+end
+
+action :delete do
+  require 'route53_a_record'
+
+  begin
+    handler.delete(record)
+    Chef::Log.debug("Destroyed record: #{r.name}")
+  rescue Exception => e
+    Chef::Log.error e.to_s
+  end
+end
